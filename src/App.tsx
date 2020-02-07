c042e81959746e44c07774998ac52499aab2799c
Yanislav <yanikkcr7@gmail.com>
2020-02-07 23:54:49 +0200

    installed react-navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React, { ReactElement } from 'react';
 import { SafeAreaView, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = (): ReactElement => {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <Text>TS Example</Text>
       </SafeAreaView>
-    </>
+    </NavigationContainer>
   );
 };
 
