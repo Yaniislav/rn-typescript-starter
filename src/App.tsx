7271d75c3a123d5d833b0fb62f4a1b4d89b82e9a
Yanislav <yanikkcr7@gmail.com>
2020-02-19 11:56:26 +0200

    added build variants to android, integrated dotenv files

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,12 @@ import React, { ReactElement } from 'react';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from 'react-redux';
 
+import Config from 'react-native-config';
+
 import store, { persistor } from './store';
 import MainNavigator from 'navigation';
+
+console.log('Api Url: ', Config.API_URL);
 
 const App = (): ReactElement => {
   return (
